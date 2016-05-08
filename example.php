f0741252289e713ebf6b2ce0148480d28f6e710c
Alex <Alehandr0@users.noreply.github.com>
2016-05-08 07:54:02 +0300

    Create example.php

---
+++ example.php
@@ -0,0 +1,19 @@
+<?	session_start();
+		$Ol=$_SESSION['Ol'];
+		$l1=strtolower($_POST[$Ol]);	
+
+if(count($_POST)>0)isset($_SESSION['kap4a'])&&$_SESSION['kap4a']===$l1?$n=1:$n=0;
+if(!isset($_SESSION['kap4a'])||!isset($_SESSION['Ol']))
+  {
+	echo"<meta http-equiv=Refresh content='0; url=http://{$_SERVER['HTTP_HOST']}/dopolnenie/kap4a/z__example.php'>";
+	$hid4a=1; require_once("../dopolnenie/kap4a/z__example.php"); exit;
+}
+?>
+<form method=POST>
+<p>Введите текст, показанный ниже:</p>
+<p><img src="http://<?=$_SERVER['HTTP_HOST']?>/dopolnenie/kap4a/index.php?s&c&r=5"></p>
+<?$lO=$_SESSION['Ol']=$_SESSION['nam4a']?>
+<p><input name=<?=$lO?> type=text size=16 maxlength=10></p>
+<p><input type=submit value='  Подтвердить ввод  '></p>
+<?=$n?"Совпадение":"Ошибка"?>
+</form>
