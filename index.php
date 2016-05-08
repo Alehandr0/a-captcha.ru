8ca49f689207a89b4899b1e2a022f6e75d724946
Alex <Alehandr0@users.noreply.github.com>
2016-05-08 11:17:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,6 @@ isset($_GET['r'])?$r=floor(abs($_GET['r'])):$r=0;       //	reduce size
 isset($_GET['c'])?$c=1:$c=0;                            //	capital letters
 isset($_GET['s'])||$sign?$s=1:$s=0;                     //	sign a picture
 
-$ini=array(0=>3, $r, $c, $s);
+$ini=array(3, $r, $c, $s);
 require_once('class.kap4a.php');
 if($_REQUEST[session_name()]){$_SESSION['kap4a']=$kap4a->kap4a; $_SESSION['nam4a']=$kap4a->nam4a;}
