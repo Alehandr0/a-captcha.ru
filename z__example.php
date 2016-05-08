af641d2f89217bda008c6f5b4e4d2fb3f1d13eff
Alex <Alehandr0@users.noreply.github.com>
2016-05-08 08:10:58 +0300

    Update z__example.php

--- z__example.php
+++ z__example.php
@@ -5,13 +5,13 @@
 if(count($_POST)>0)isset($_SESSION['kap4a'])&&$_SESSION['kap4a']===$l1?$n=1:$n=0;
 if(!isset($_SESSION['kap4a'])||!isset($_SESSION['Ol']))
   {
-	echo"<meta http-equiv=Refresh content='0; url=http://{$_SERVER['HTTP_HOST']}/dopolnenie/kap4a/z__example.php'>";
-	$hid4a=1; require_once("../dopolnenie/kap4a/z__example.php"); exit;
+	echo"<meta http-equiv=Refresh content='0; url=http://{$_SERVER['HTTP_HOST']}/kap4a/z__example.php'>";
+	$hid4a=1; require_once("z__example.php"); exit;
 }
 ?>
 <form method=POST>
 <p>Введите текст, показанный ниже:</p>
-<p><img src="http://<?=$_SERVER['HTTP_HOST']?>/dopolnenie/kap4a/index.php?s&c&r=5"></p>
+<p><img src="http://<?=$_SERVER['HTTP_HOST']?>/kap4a/index.php?s&c&r=5"></p>
 <?$lO=$_SESSION['Ol']=$_SESSION['nam4a']?>
 <p><input name=<?=$lO?> type=text size=16 maxlength=10></p>
 <p><input type=submit value='  Подтвердить ввод  '></p>
